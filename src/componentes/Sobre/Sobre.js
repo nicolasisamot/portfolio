080bcedd5a68473c8dd3a3e2ce5d64c308f1d2f9
nicolasisamot <nicolasisamot@gmail.com>
2024-10-09 19:12:05 -0300

    att

--- src/componentes/Sobre/Sobre.js
+++ src/componentes/Sobre/Sobre.js
@@ -7,8 +7,8 @@ export default function Sobre() {
       <div className={styles.container}>
         <img className={styles.foto} src="./images/programming.png"></img>
         <p className={styles.text}>
-          Sou desenvolvedor Full Stack, com experiência em HTML, CSS, JavaScript
-          e ReactJS. Trabalho com bancos de dados como MySQL, utilizando
+          Desenvolvedor Full Stack, com experiência em HTML, CSS, JavaScript e
+          ReactJS. Trabalho com bancos de dados como MySQL, utilizando
           Sequelize, e MongoDB, com Mongoose. Tenho experiência na criação de
           APIs REST, utilizando ExpressJS, além de implementar tokens JWT para
           autenticação e hasheamento de senhas. Também desenvolvo projetos que
